7b7e6b72f5c8a6c92c214014bfa750d146c559fd
Dzkzzz <dzdzaakk@gmail.com>
2023-07-31 21:14:32 +0700

    practice until number 15

--- sql-exercises/Aggregate Functions/sql.sql
+++ sql-exercises/Aggregate Functions/sql.sql
@@ -51,4 +51,23 @@ GROUP BY salesman_id;
 #12 From the following table, write a SQL query to find the highest order (purchase) amount by each customer on a particular order date. Filter the result by highest order (purchase) amount above 2000.00. Return customer id, order date and maximum purchase amount. 
 SELECT customer_id, ord_date, MAX(purch_amt) 
 FROM orders
-GROUP BY customer_id, MAX(purch_amt) >2000;
+GROUP BY customer_id, ord_date
+HAVING MAX(purch_amt)>2000.00;
+
+#13 From the following table, write a SQL query to find the maximum order (purchase) amount in the range 2000 - 6000 (Begin and end values are included.) by combination of each customer and order date. Return customer id, order date and maximum purchase amount. 
+SELECT customer_id, ord_date, MAX(purch_amt) 
+FROM orders
+GROUP BY customer_id, ord_date
+HAVING MAX(purch_amt) BETWEEN 2000 AND 6000;
+
+#14 From the following table, write a SQL query to find the maximum order (purchase) amount based on the combination of each customer and order date. Filter the rows for maximum order (purchase) amount is either 2000, 3000, 5760, 6000. Return customer id, order date and maximum purchase amount
+SELECT customer_id, ord_date, MAX(purch_amt)
+FROM orders
+GROUP BY customer_id, ord_date
+HAVING MAX(purch_amt) IN(2000, 3000, 5760, 6000);
+
+#15 From the following table, write a SQL query to determine the maximum order amount for each customer. The customer ID should be in the range 3002 and 3007(Begin and end values are included.). Return customer id and maximum purchase amount. 
+SELECT customer_id, MAX(purch_amt)
+FROM orders
+WHERE customer_id BETWEEN 3002 AND 3007
+GROUP BY customer_id;
